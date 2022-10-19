1f722796fccc50afa272e383af76d8d6c0a22f47
Guilherme <guilhermeneis@live.com>
2022-10-19 17:10:40 -0300

    Adição da tela de cadastro e fluxo do app

--- app/src/main/java/com/example/carapp/ui/user/LoginActivity.kt
+++ app/src/main/java/com/example/carapp/ui/user/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.carapp.ui
+package com.example.carapp.ui.user
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.carapp.MainActivity
 import com.example.carapp.R
 
@@ -14,10 +15,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login_content)
 
         val btLogin = findViewById<Button>(R.id.btLogin)
+        val etCliqueAqui = findViewById<TextView>(R.id.tvCliqueAqui)
 
         btLogin.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        etCliqueAqui.setOnClickListener{
+            startActivity(Intent(this, SignActivity::class.java))
+        }
+
 
     }
 
