0ff06c1a53989be71c83a7fb2123be8833f4c3a6
Guilherme <guilhermeneis@live.com>
2022-10-25 15:49:16 -0300

    Commit teste

--- app/src/main/java/com/example/carapp/model/Drink.kt
+++ app/src/main/java/com/example/carapp/model/Drink.kt
@@ -4,9 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity
 data class Drink(
-    @PrimaryKey
     @SerializedName("id") val id: Int,
     @SerializedName("strDrink") val strDrink: String,
     @SerializedName("strInstructions") val strInstructions: String,
